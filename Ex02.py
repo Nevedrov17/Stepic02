a9a6967cc2163652bc883c41b9607bc7980a018e
NEVA <neva.vka@yandex.ru>
2022-12-15 22:04:17 +0300

    Задания под блоком 2

---
+++ Ex02.py
@@ -0,0 +1,47 @@
+# print('I', 'like', 'Python', sep='**')
+
+# name1 = input()
+# name2 = input()
+# name3 = input()
+# name4 = input()
+# print(name1,name2,name3, sep=name4)
+
+# name = input()
+# print('Привет,', name, sep=" ", end='!')
+
+# a = int(input())
+# print(a)
+# print(a+1)
+# print(a+2)
+
+# num = int(input())
+# print('Объем =', num*num*num)
+# print('Площадь полной поверхности =', 6*num*num)
+
+# a = int(input())
+# b = int(input())
+# print(3 * (a + b) * (a + b) * (a + b) + 275 * b * b - 127 * a - 41)
+
+# a = int(input())
+# print("Следующее за числом", a,  "число:", a+1)
+# print("Для числа", a, "предыдущее число:", a-1)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# print((a+b+c+d)*3)
+
+# a = int(input())
+# b = int(input())
+# print(str(a), '+', str(b), '=', a+b)
+# print(str(a), '-', str(b), '=', a-b)
+# print(str(a), '*', str(b), '=', a*b)
+
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# print(a1 + d * (n - 1))
+
+a = int(input())
+print(a, 2*a, 3*a, 4*a, 5*a, sep='---')
